d3b73a30e0009e965ca00e881cb1fd6cf03fcaf9
jjmrbldz <jejo.baldoza@gmail.com>
2024-05-06 09:33:49 +0800

    update api doc

--- src/views/APIDocumentationView.vue
+++ src/views/APIDocumentationView.vue
@@ -7,11 +7,12 @@
 <script>
 import apidoc from '@/assets/SmartApi_SingleWallet_2024-v0.60.pdf'
 import apidoc2 from '@/assets/Kosca_SingleWallet_2024-v0.7 (kr).pdf'
+import apidoc3 from '@/assets/KoscaApi_Seamless_2024-v1.02 (kr).pdf'
 
 export default {
     data() {
         return {
-            pdfUrl: apidoc2,
+            pdfUrl: apidoc3,
         }
     },
 }
