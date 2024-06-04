0cf0c0518d79be65c9c8d0cb16298443a4c8f82c
jjmrbldz <jejo.baldoza@gmail.com>
2024-06-04 14:40:16 +0800

    modify pp

--- src/components/Modals/CasinoBetDetailsModal/PragmaticDetails/PragmaticDetails.vue
+++ src/components/Modals/CasinoBetDetailsModal/PragmaticDetails/PragmaticDetails.vue
@@ -1,6 +1,6 @@
 <template>
     <Fieldset :legend="$store.getters['languageStore/translate'](`Game Result`)" :toggleable="true">
-        <ResultLink :agent-ID="betData.partner_username" :game-ID="betData.provider_id" :tx-ID="betData1.transactionId" />
+        <ResultLink :agent-ID="betData.partner_username" :game-ID="betData.provider_id" :tx-ID="betData.txid" />
     </Fieldset>
 </template>
 
@@ -13,13 +13,13 @@ export default {
     },
     data() {
         return {
-            betData1: this.otherDetails ? JSON.parse(this.otherDetails.betData) : null,
-            betData2: this.resultDetails ? JSON.parse(this.resultDetails.betData) : null
+            // betData1: this.otherDetails ? JSON.parse(this.otherDetails.betData) : null,
+            // betData2: this.resultDetails ? JSON.parse(this.resultDetails.betData) : null
         }
     },
     mounted() {
-        console.log(this.otherDetails, this.betData1);
-        console.log(this.resultDetails, this.betData2);
+        // console.log(this.otherDetails, this.betData1);
+        // console.log(this.resultDetails, this.betData2);
     },
     methods: {
         
