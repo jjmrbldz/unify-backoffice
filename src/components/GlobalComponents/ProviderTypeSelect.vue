f76ea55888dde0557ae7da11b95ea5372caa366a
jjmrbldz <jejo.baldoza@gmail.com>
2024-06-24 11:44:11 +0800

    add pgsoft

--- src/components/GlobalComponents/ProviderTypeSelect.vue
+++ src/components/GlobalComponents/ProviderTypeSelect.vue
@@ -56,6 +56,7 @@ export default {
                         {label: this.$store.getters['languageStore/translate']('All'), value: ''},
                         {label: this.$store.getters['languageStore/translate']('Pragmatic'), value: 'pp'},
                         {label: this.$store.getters['languageStore/translate']('Microgaming'), value: 'micro'},
+                        {label: this.$store.getters['languageStore/translate']('PG Soft'), value: 'pgsoft'},
                     ]
                     this.$emit('update:modelValue', '');
                 } else if (newVal === 'minigame') {
