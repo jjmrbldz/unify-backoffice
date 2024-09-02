411a854653bc6f6ae49d1e308dcce5aa9a561eff
jjmrbldz <jejo.baldoza@gmail.com>
2024-09-02 21:29:35 +0800

    same same betsum on all histories

--- src/components/Pages/History/BetSumm.vue
+++ src/components/Pages/History/BetSumm.vue
@@ -1,19 +1,6 @@
 <template>
-    <div v-if="$route.query.bettype === 'sport'" class="flex align-items-center gap-6 text-sm text-600 ml-auto">
-        <div class="font-bold">{{ $store.getters['languageStore/translate']('Total Unsettled') }}: </div>
-        <div class="text-center">
-            <div class="font-bold">{{ $store.getters['languageStore/translate']('Pending Bet') }}</div>
-            <div class="" :class="this.$GF.handleTextColor($store.state.betSummStore.betSumm.totalPendingBet)">{{ $GF.formatNumComma($store.state.betSummStore.betSumm.totalPendingBet) }}</div>
-        </div>
-        <div class="text-center">
-            <div class="font-bold">{{ $store.getters['languageStore/translate']('Expected Winning') }}</div>
-            <div class="" :class="this.$GF.handleTextColor($store.state.betSummStore.betSumm.totalExpectedWinning)">{{ $GF.formatNumComma($store.state.betSummStore.betSumm.totalExpectedWinning) }}</div>
-        </div>
-    </div>
-    <Divider class="mx-6" v-if="$route.query.bettype === 'sport'" layout="vertical" />
     <div class="flex align-items-center gap-6 text-sm text-600">
-        <div v-if="$route.query.bettype === 'sport'" class="font-bold">{{ $store.getters['languageStore/translate']('Settlement Completed') }}: </div>
-        <div v-else class="font-bold">{{ $store.getters['languageStore/translate']('Sum') }}: </div>
+        <div class="font-bold">{{ $store.getters['languageStore/translate']('Sum') }}: </div>
         <div class="text-center">
             <div class="font-bold">{{ $store.getters['languageStore/translate']('Bet') }}</div>
             <div class="" :class="this.$GF.handleTextColor($store.state.betSummStore.betSumm.totalBet)">{{ $GF.formatNumComma($store.state.betSummStore.betSumm.totalBet) }}</div>
